d87f778238d55ff968d72826b292fb0b77300f1c
Ritviz <1822mritviz@gmail.com>
2023-07-08 13:10:51 +0530

    textutils wrapped

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -121,7 +121,7 @@ export default function TextForm(props) {
         <p>
           Your text contains{" "}
           {
-            text.split(" ").filter((element) => {
+            text.split(/\s+/).filter((element) => {
               return element.length !== 0;
             }).length
           }{" "}
